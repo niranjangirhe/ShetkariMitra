6b25ff8f3e88355e2c95dc39afe1a7a67c15cb5b
niranjangirhe <niranjangirheindia@gmail.com>
2021-04-22 00:26:18 +0530

    Text Suggestion fixed

--- app/src/main/java/com/ngsolutions/myapplication/HelperClasses/HomeAddapter/ReplyAdapter.java
+++ app/src/main/java/com/ngsolutions/myapplication/HelperClasses/HomeAddapter/ReplyAdapter.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
